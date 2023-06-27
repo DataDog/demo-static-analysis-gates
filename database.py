a628ccfa792aa04e901e1ef53dfe463f58578030
Andrea Moscatelli <andrea.moscatelli@datadoghq.com>
2023-06-27 15:59:25 +0200

    Demo adding product ID

--- database.py
+++ database.py
@@ -6,10 +6,10 @@ def connect_database():
     con = sqlite3.connect(DATABASE_FILE, check_same_thread=False)
     return con
 
-def get_products(db_connection, limit, offset):
+def get_products(db_connection, limit, offset, product_id):
     products = []
-    cursor = db_connection.cursor();
-    res = cursor.execute("SELECT id, title from products");
+    cursor = db_connection.cursor()
+    res = cursor.execute(f"SELECT id, title from products where id={product_id}")
     for v in res:
         products.append(Product(v[0], v[1]))
 
--- service.py
+++ service.py
@@ -5,11 +5,12 @@ app = Flask(__name__)
 
 db_connection = db.connect_database()
 
-@app.route("/product/list", methods=["GET"])
-def product_list():
+
+@app.route("/product/list/<product_id>", methods=["GET"])
+def product_list(product_id):
     limit = int(request.args.get('limit', 10))
     offset = int(request.args.get('offset', 0))
-    products = db.get_products(db_connection, limit, offset)
+    products = db.get_products(db_connection, limit, offset, product_id)
     return jsonify(products)
 
 
