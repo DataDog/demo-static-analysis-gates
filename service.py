df48844fdefe7d0c49ff7203e39aeb076472d7be
Andrea Moscatelli <andrea.moscatelli@datadoghq.com>
2023-06-27 16:15:34 +0200

    Demo test - Add product ID

--- service.py
+++ service.py
@@ -5,9 +5,16 @@ app = Flask(__name__)
 
 db_connection = db.connect_database()
 
+
 @app.route("/product/list", methods=["GET"])
 def product_list():
     products = db.get_products(db_connection)
+    return jsonify(products)
+
+
+@app.route("/product/<product_id>", methods=["GET"])
+def product_list_by_id(product_id):
+    products = db.get_product_by_id(db_connection, product_id)
     return jsonify(products)
 
 
